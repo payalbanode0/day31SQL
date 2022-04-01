04bc67db6515a05b1c2ec23c60d016f9f4bc203e
payalbanode0 <payalbanode0@gmail.com>
2022-04-01 23:34:07 +0530

    added where to between condition

--- payroll.sql
+++ payroll.sql
@@ -1,10 +1,15 @@
 use payrollservices
 Go
 
-Insert into employee_payroll (Name,Salary,StartDate) values('payal',80000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('pooja',13000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('priya',5000000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
 
-select *
-from employee_payroll
+Select Salary 
+From employee_payroll
+Where Name='Payal'
 Go
+
+Select *
+From employee_payroll
+Where StartDate Between Cast('2018-01-01' As Date) And GETDATE() 
