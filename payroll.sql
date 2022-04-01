701eb2150c87dcc46b3322d043d92dfd359408a0
payalbanode0 <payalbanode0@gmail.com>
2022-04-01 23:30:43 +0530

    added view data

--- payroll.sql
+++ payroll.sql
@@ -1,8 +1,10 @@
---uc3 insert data into table
 use payrollservices
 Go
 
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('payal',80000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('pooja',13000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('priya',5000000,GETDATE())
 
+select *
+from employee_payroll
+Go
