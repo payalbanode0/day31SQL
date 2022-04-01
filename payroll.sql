7c90bd00164f38380002a025df1aa63f4bf170a7
payalbanode0 <payalbanode0@gmail.com>
2022-04-01 23:40:23 +0530

    update data

--- payroll.sql
+++ payroll.sql
@@ -1,15 +1,31 @@
 use payrollservices
 Go
 
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('payal',80000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('pooja',13000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('priya',5000000,GETDATE())
 
 Select Salary 
 From employee_payroll
-Where Name='Payal'
+Where Name='payal'
 Go
 
 Select *
 From employee_payroll
 Where StartDate Between Cast('2018-01-01' As Date) And GETDATE() 
+
+Alter Table employee_payroll
+Add Gender char
+Go
+
+Update employee_payroll
+Set Gender = 'f' 
+Where Name = 'priya' or Name = 'Manu'
+Go
+
+Update employee_payroll
+Set Gender = 'f' 
+Where Name = 'pooja'
+Go
+Select *
+From employee_payroll
