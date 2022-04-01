60a8d2698558ba88eb2366d608a8905fdf68f850
payalbanode0 <payalbanode0@gmail.com>
2022-04-01 23:21:09 +0530

    added create table

--- payroll.sql
+++ payroll.sql
@@ -1,6 +1,15 @@
---UC1 : Ability to create payroll service 
-
+--uc2 create table
 create database payroll_service
 Go
 use payroll_service
+Go
+Create Table employee_payroll
+( Id int Identity primary Key,
+  Name varchar(50) Not Null,
+  Salary money default 1000,
+  StartDate datetime default GetDate()
+) 
+
+select *
+from employee_payroll
 Go
