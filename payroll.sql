102be0a125d86228f4146552d14fb6fa1239a2aa
payalbanode0 <99254852+payalbanode0@users.noreply.github.com>
2022-04-01 23:29:18 +0530

    Update payroll.sql

--- payroll.sql
+++ payroll.sql
@@ -2,7 +2,7 @@
 use payrollservices
 Go
 
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('payal',80000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('pooja',13000,GETDATE())
+Insert into employee_payroll (Name,Salary,StartDate) values('priya',5000000,GETDATE())
 
